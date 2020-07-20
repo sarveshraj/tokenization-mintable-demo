75a562e5b1469152660f728efcb53300ce8b5de5
Sarvesh Raj <sarraj@adobe.com>
2020-07-20 23:57:52 +0530

    Initial commit

---
+++ contracts/MyTokenSale.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.6.1;
+
+import "./Crowdsale.sol";
+import "./KycContract.sol";
+import "./MintedCrowdsale.sol";
+
+contract MyTokenSale is MintedCrowdsale {
+
+    KycContract kyc;
+
+    constructor(
+        uint256 rate,    // rate in TKNbits
+        address payable wallet,
+        IERC20 token,
+        KycContract _kyc
+    )
+        MintedCrowdsale()
+        Crowdsale(rate, wallet, token)
+        public
+    {
+        kyc = _kyc;
+
+    }
+
+    function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal view override {
+        super._preValidatePurchase(beneficiary, weiAmount);
+        require(kyc.kycCompleted(msg.sender), "KYC Not completed, purchase not allowed");
+    }
+}
